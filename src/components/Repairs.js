3cb49f04eb5f0a9942cd511744fbe97b1c19a27d
jmrosenberger <jmrosenberger0811@yahoo.com>
2021-08-30 14:20:17 -0500

    chapter 9 completed

--- src/components/Repairs.js
+++ src/components/Repairs.js
@@ -1,18 +1,33 @@
-import React from "react"
-import { NavBar } from "./nav/NavBar.js"
-import { ApplicationViews } from "./ApplicationViews.js"
-import "./Repairs.css"
- // ------  || THIS MODULE IS FOR DETERMINING WHAT ORDER EVERYTHING RENDERS ||  ----- //
-// ------   ||    IN HTML. ALL FUNCTIONALITY SHOULD GO IN OTHER MODULES.    || ----- //
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { ApplicationViews } from "./ApplicationViews";
+import { NavBar } from "./nav/NavBar";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
+import "./Repairs.css";
 
+export const Repairs = () => (
+  <>
+    <Route
+      render={() => {
+        if (localStorage.getItem("honey_customer")) {
+          return (
+            <>
+              <NavBar />
+              <ApplicationViews />
+            </>
+          );
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
 
-export const Repairs = () => {
-    
-    return (
-        <>
-        <NavBar />
-        <h1>Honey Rae's Repair Shop</h1>
-        <ApplicationViews />
-        </>
-    )
-}
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
+  </>
+);
